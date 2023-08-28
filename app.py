9502013448a238ec59c86dcd4b38ec02d0dcb2a5
Michael H. Oshita <ijinpublic+github@gmail.com>
2023-08-27 22:22:35 -0700

    show playa name

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ def sink():
 @app.route('/selection', methods = ['GET'])
 def selection():
     u = User.query.get(session['id'])
-    return render_template('selection.html', credits=u.credits, name=u.name)
+    return render_template('selection.html', credits=u.credits, name=u.name, pi_name=u.pi_name)
 
 @app.route('/instructions', methods = ['POST', 'GET'])
 def instructions():
